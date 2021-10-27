4841069fae21e335ba664f98c50ba98fa1a4b3b3
Greg Hoin <ghoin@hashicorp.com>
2021-10-27 15:46:42 +0200

    import theme separately to override default in vendor

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -55,5 +55,7 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import('app/styles/prism-dracula.css');
+
   return app.toTree();
 };
